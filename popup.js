42512a8ebd5dcf6596e73fa60d66239754bb7377
KarolannMauger <karolanngmail.com>
2023-10-15 11:56:30 -0400

    HTTPS DONE

--- popup.js
+++ popup.js
@@ -1,5 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById("checkCookies").addEventListener("click", function() {
-        console.log("Le bouton a été cliqué !");
-    });
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    var currentTab = tabs[0];
+    var currentUrl = new URL(currentTab.url);
+    console.log(currentUrl);
+    if(currentUrl.protocol === "https:"){
+        console.log("good protocol");
+        document.getElementById("https_").style.backgroundColor = "green";
+    } else {
+        console.log("bad protocol");
+        document.getElementById("https_").style.backgroundColor = "red";
+    }
 });
